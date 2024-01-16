4e21b682f0c683bc263accfa73aa0bbaaec7f283
Jay <124623992+jay9358@users.noreply.github.com>
2024-01-17 01:01:21 +0530

    sd

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Preloader.css';
 
 
 function Preloader() {
   
+
   return (
     <div className="preloader">
         <div class="spinner">
